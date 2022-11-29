34b54d060998e79a4d367bfb7345360163433b1c
topdg <dev.topdg@bk.ru>
2022-11-29 20:50:52 +0300

    making metrkia goals via eval

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,5 @@ export const cls = (...classNames: (string | boolean | undefined | null)[]) =>
   classNames.filter(Boolean).join(" ");
 
 export const yaMetrikaGoal = (goalName : string) => {
-  console.log('ya metrika reach goal ', goalName);
-  // eval(`ym(91414803, 'reachGoal', ${goalName})`)
+  eval(`ym(91414803, 'reachGoal', ${goalName})`)
 };
