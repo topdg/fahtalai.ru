4cba4c096a5bcee91c51a5c01af7cdaeb26bafb3
topdg <dev.topdg@bk.ru>
2022-11-30 15:34:32 +0300

    tring to call ym method from window object

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,5 +2,5 @@ export const cls = (...classNames: (string | boolean | undefined | null)[]) =>
   classNames.filter(Boolean).join(" ");
 
 export const yaMetrikaGoal = (goalName : string) => {
-  eval(`ym(91414803, 'reachGoal', '${goalName}')`)
+  window.ym(91414803, 'reachGoal', goalName);
 };
